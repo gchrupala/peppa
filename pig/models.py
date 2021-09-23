b5afde696f6a8d6c65b9ea5d26a4adff499bfdf9
Grzegorz Chrupała <g.chrupala@uvt.nl>
2021-09-23 17:17:43 +0200

    Messing with learning rate. This version (40 in logs) begins to fit.

--- pig/models.py
+++ pig/models.py
@@ -12,69 +12,34 @@ from torchvision.transforms import Compose
 from pig.loss import TripletLoss
 import pig.data
 import logging
-import fairseq
 
 
-class Identity(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return x
-    
 ## Audio encoders
 
 class Wav2LetterEncoder(nn.Module):
     
-    def __init__(self, project=False):
+    def __init__(self):
         super().__init__()
         self.audio = A.Wav2Letter(input_type='waveform', num_features=1, num_classes=512)
         self.audiopool = torch.nn.AdaptiveAvgPool2d((512,1))
-        if project:
-            self.project = nn.Linear(512, 512)
-        else:
-            self.project = Identity()
-        
+
     def forward(self, x):
         return Compose([self.audio.acoustic_model,
                         self.audiopool,
                         lambda x: x.squeeze(),
-                        self.project,
                         lambda x: nn.functional.normalize(x, p=2, dim=1)
         ])(x)
 
 
-class Wav2VecSmall(nn.Module):
-    # FIXME This is currently broken
-    def __init__(self, path):
-        super().__init__()
-        self.path = path
-        models, _, _ = fairseq.checkpoint_utils.load_model_ensemble_and_task([self.path])
-        self.audio = models[0]
-        self.audiopool = torch.nn.AdaptiveAvgPool2d((512,1))
-        self.project = nn.Linear(512, 512)
-        
-    def forward(self, x):
-        return Compose([self.audio.feature_extractor,
-                        lambda x: x.squeeze(),
-                        self.project,
-                        lambda x: nn.functional.normalize(x, p=2, dim=1)
-        ])(x)
 
-
-        
 ## Video encoders
 class R3DEncoder(nn.Module):
     
-    def __init__(self, pretrained=False, project=False):
+    def __init__(self, pretrained=False):
         super().__init__()
         self.pretrained = pretrained
         self.video = V.r3d_18(pretrained=pretrained, progress=False)
-        if project:
-            self.project = nn.Linear(512, 512)
-        else:
-            self.project = Identity()
-        
+
     def forward(self, x):
         return Compose([self.video.stem,
                         self.video.layer1,
@@ -83,7 +48,6 @@ class R3DEncoder(nn.Module):
                         self.video.layer4,
                         self.video.avgpool,
                         lambda x: x.flatten(1),
-                        self.project,
                         lambda x: nn.functional.normalize(x, p=2, dim=1)
         ])(x)
     
@@ -128,7 +92,7 @@ class PeppaPig(pl.LightningModule):
     #def test_step(self, batch, batch_idx):    
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
         return optimizer
 
 
@@ -136,13 +100,11 @@ class PeppaPig(pl.LightningModule):
     
 def main():
 
-    logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger().setLevel(logging.INFO)
     data = pig.data.PigData(extract=False, prepare=False, normalization='kinetics')
-
-    audio_encoder = Wav2LetterEncoder(project=False)
-    video_encoder = R3DEncoder(pretrained=True, project=False)
     
-    net = PeppaPig(audio_encoder=audio_encoder, video_encoder=video_encoder)
+    net = PeppaPig(audio_encoder=Wav2LetterEncoder(),
+                   video_encoder=R3DEncoder(pretrained=True))
 
     trainer = pl.Trainer(gpus=3, overfit_batches=10, log_every_n_steps=10, limit_val_batches=0)
     
