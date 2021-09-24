1845a994936f995567a09e748a8fb11c402eee68
Grzegorz Chrupała <g.chrupala@uvt.nl>
2021-09-24 12:48:31 +0200

    Cleaner config.

--- pig/models.py
+++ pig/models.py
@@ -14,19 +14,26 @@ import pig.data
 import logging
 
 
+def identity(x):
+    return x
 ## Audio encoders
 
 class Wav2LetterEncoder(nn.Module):
     
-    def __init__(self):
+    def __init__(self, project=False):
         super().__init__()
         self.audio = A.Wav2Letter(input_type='waveform', num_features=1, num_classes=512)
         self.audiopool = torch.nn.AdaptiveAvgPool2d((512,1))
+        if project:
+            self.project = nn.Linear(512, 512)
+        else:
+            self.project = identity
 
     def forward(self, x):
         return Compose([self.audio.acoustic_model,
                         self.audiopool,
                         lambda x: x.squeeze(),
+                        self.project,
                         lambda x: nn.functional.normalize(x, p=2, dim=1)
         ])(x)
 
@@ -35,11 +42,16 @@ class Wav2LetterEncoder(nn.Module):
 ## Video encoders
 class R3DEncoder(nn.Module):
     
-    def __init__(self, pretrained=False):
+    def __init__(self, pretrained=False, project=False):
         super().__init__()
         self.pretrained = pretrained
         self.video = V.r3d_18(pretrained=pretrained, progress=False)
+        if project:
+            self.project = nn.Linear(512, 512)
+        else:
+            self.project = identity
 
+        
     def forward(self, x):
         return Compose([self.video.stem,
                         self.video.layer1,
@@ -48,16 +60,20 @@ class R3DEncoder(nn.Module):
                         self.video.layer4,
                         self.video.avgpool,
                         lambda x: x.flatten(1),
+                        self.project,
                         lambda x: nn.functional.normalize(x, p=2, dim=1)
         ])(x)
     
     
 class PeppaPig(pl.LightningModule):
-    def __init__(self, audio_encoder, video_encoder):
+    def __init__(self, config):
         super().__init__()
-        self.video_encoder = video_encoder
-        self.audio_encoder = audio_encoder
-        self.loss = TripletLoss(margin=0.2)
+        self.config = config
+        self.save_hyperparameters(config)
+        self.loss = TripletLoss(margin=self.config['margin'])
+        self.audio_encoder=Wav2LetterEncoder(project=self.config['audio']['project'])
+        self.video_encoder=R3DEncoder(pretrained=self.config['video']['pretrained'],
+                                      project=self.config['video']['project'])
         
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
@@ -92,7 +108,7 @@ class PeppaPig(pl.LightningModule):
     #def test_step(self, batch, batch_idx):    
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config['lr'])
         return optimizer
 
 
@@ -101,13 +117,19 @@ class PeppaPig(pl.LightningModule):
 def main():
 
     logging.getLogger().setLevel(logging.INFO)
-    data = pig.data.PigData(extract=False, prepare=False, normalization='kinetics')
-    
-    net = PeppaPig(audio_encoder=Wav2LetterEncoder(),
-                   video_encoder=R3DEncoder(pretrained=True))
+                                      
+    config = dict(lr=1e-5,
+                  margin=0.1,
+                  video=dict(pretrained=False, project=True),
+                  audio=dict(project=True))
+                                      
+    data = pig.data.PigData(extract=False,
+                            prepare=False,
+                            normalization='kinetics' if config['video']['pretrained'] else 'peppa')
+    net = PeppaPig(config)
 
-    trainer = pl.Trainer(gpus=3, overfit_batches=10, log_every_n_steps=10, limit_val_batches=0)
     
+    trainer = pl.Trainer(gpus=3, overfit_batches=10, log_every_n_steps=10, limit_val_batches=0)
     trainer.fit(net, data)
 
 if __name__ == '__main__':
