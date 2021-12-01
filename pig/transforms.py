365510084dbc2ea4c1fba05ad58d07aacd73cfb8
Grzegorz Chrupała <g.chrupala@uvt.nl>
2021-12-01 17:07:09 +0100

    Adapt transform to batched data

--- pig/transforms.py
+++ pig/transforms.py
@@ -5,4 +5,4 @@ import torch.nn as nn
 class SwapCT(nn.Module):
 
     def forward(self, vid: torch.Tensor) -> torch.Tensor:
-        return vid.permute(1, 0, 2, 3)
+        return vid.permute(0, 2, 1, 3, 4)
