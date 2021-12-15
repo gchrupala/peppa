215763807a884ae0744a73fc3ee85dfc5d58d678
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-12-15 11:08:01 +0100

    Improve targeted_triplets_test.py

--- pig/targeted_triplets_test.py
+++ pig/targeted_triplets_test.py
@@ -3,44 +3,55 @@ import time
 import numpy as np
 from moviepy.audio.AudioClip import AudioArrayClip
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
-from torch.utils.data import DataLoader
 
 from pig.targeted_triplets import PeppaTargetedTripletDataset, FPS
 import pygame
-
-from pig.triplet import collate_triplets
+import pandas as pd
 
 AUDIO_SAMPLE_RATE = 44100  # Sampling rate in samples per second.
 
 
 if __name__ == "__main__":
     fragment_type = "narration"
-    pos = "ADJ"
+    pos = "NOUN"
+
+    target_word = "glass"
+
+    eval_info_file = f"data/eval/eval_set_{fragment_type}_{pos}.csv"
+    eval_info = pd.read_csv(eval_info_file, index_col="id")
 
     eval_dataset = PeppaTargetedTripletDataset.load(f"data/out/val_{fragment_type}_targeted_triplets_{pos}")
-    loader = DataLoader(eval_dataset, collate_fn=collate_triplets, batch_size=1)
 
-    for batch in loader:
-        video_data_pos = batch.positive
-        video_data_pos = video_data_pos.squeeze(0).permute(1, 2, 3, 0) * 255
-        video_data_pos = list(video_data_pos.numpy())
-        video_clip_pos = ImageSequenceClip(video_data_pos, fps=FPS)
+    assert len(eval_info) == len(eval_dataset)
 
-        video_data_neg = batch.negative
-        video_data_neg = video_data_neg.squeeze(0).permute(1, 2, 3, 0) * 255
-        video_data_neg = list(video_data_neg.numpy())
-        video_clip_neg = ImageSequenceClip(video_data_neg, fps=FPS)
+    for i, info in eval_info.iterrows():
+        if info["target_word"] == target_word:
+            print(eval_info.iloc[i]["transcript"], end=" | ")
+            print("Target: ", eval_info.iloc[i]["target_word"], end=" | ")
+            print("Distractor: ", eval_info.iloc[i]["distractor_word"])
 
-        audio_data = batch.anchor
-        audio_data = audio_data.squeeze(0).permute(1, 0).numpy()
+            sample = eval_dataset.__getitem__(i)
 
-        audio_data = np.tile(audio_data, 2)     # Playing mono doesn't work
+            video_data_pos = sample.positive
+            video_data_pos = video_data_pos.permute(1, 2, 3, 0) * 255
+            video_data_pos = list(video_data_pos.numpy())
+            video_clip_pos = ImageSequenceClip(video_data_pos, fps=FPS)
 
-        audio_clip = AudioArrayClip(audio_data, fps=AUDIO_SAMPLE_RATE)
+            video_data_neg = sample.negative
+            video_data_neg = video_data_neg.permute(1, 2, 3, 0) * 255
+            video_data_neg = list(video_data_neg.numpy())
+            video_clip_neg = ImageSequenceClip(video_data_neg, fps=FPS)
 
-        video_clip_pos = video_clip_pos.set_audio(audio_clip)
-        pygame.display.set_caption('Positive Sample')
+            audio_data = sample.anchor
+            audio_data = audio_data.permute(1, 0).numpy()
 
-        video_clip_pos.preview(fps=FPS)
+            audio_data = np.tile(audio_data, 2)     # Playing mono doesn't work
 
-        time.sleep(1)
+            audio_clip = AudioArrayClip(audio_data, fps=AUDIO_SAMPLE_RATE)
+
+            video_clip_pos = video_clip_pos.set_audio(audio_clip)
+            pygame.display.set_caption('Positive Sample')
+
+            video_clip_pos.preview(fps=FPS)
+
+            time.sleep(1)
