0a700fd8f9cdbc1bd5bc214a1c69c1db5fff1b10
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-01-25 17:35:05 +0100

    Multiprocessing for targeted triplet generation

--- generate_targeted_triplets_eval_sets.py
+++ generate_targeted_triplets_eval_sets.py
@@ -4,6 +4,7 @@ import json
 import os
 import re
 from collections import Counter
+from multiprocessing import Pool
 
 import pandas as pd
 from spacy.tokens import Doc
@@ -243,107 +244,120 @@ def crop_and_create_example(example, start, end, target_word, distractor_word):
     return example
 
 
-def find_minimal_pairs(tuples, data, args):
-    eval_set = []
-    id = 0
-    for lemma_1, lemma_2 in tqdm(tuples):
-        used_counterexamples = []
-        print(f"\nLooking for: {(lemma_1, lemma_2)}")
-        for _, s1 in data.iterrows():
-            best_example, best_counterexample, best_counterex_row = None, None, None
-            len_longest_intersection = 0
+def find_minimal_pairs_for_tuple(tuple, data, args):
+    results = []
+    lemma_1, lemma_2 = tuple
+    used_counterexamples = []
+    print(f"\nLooking for: {(lemma_1, lemma_2)}")
+    for _, s1 in data.iterrows():
+        best_example, best_counterexample, best_counterex_row = None, None, None
+        len_longest_intersection = 0
 
-            if lemma_1 in s1["lemmatized"]:
-                example_candidate = s1.copy()
-                s1_masked = [
-                    w if lemma != lemma_1 else TOKEN_MASK
+        if lemma_1 in s1["lemmatized"]:
+            example_candidate = s1.copy()
+            s1_masked = [
+                w if lemma != lemma_1 else TOKEN_MASK
+                for w, lemma in zip(
+                    example_candidate["tokenized"], example_candidate["lemmatized"]
+                )
+            ]
+            for row_counterexample, s2 in data.iterrows():
+                if row_counterexample in used_counterexamples:
+                    continue
+
+                if lemma_2 not in s2["lemmatized"]:
+                    continue
+
+                counterexample_candidate = s2.copy()
+                s2_masked = [
+                    w if lemma != lemma_2 else TOKEN_MASK
                     for w, lemma in zip(
-                        example_candidate["tokenized"], example_candidate["lemmatized"]
+                        counterexample_candidate["tokenized"],
+                        counterexample_candidate["lemmatized"],
                     )
                 ]
-                for row_counterexample, s2 in data.iterrows():
-                    if row_counterexample in used_counterexamples:
-                        continue
 
-                    if lemma_2 not in s2["lemmatized"]:
-                        continue
+                intersection = longest_intersection(s1_masked, s2_masked)
+                if not intersection:
+                    continue
 
-                    counterexample_candidate = s2.copy()
-                    s2_masked = [
-                        w if lemma != lemma_2 else TOKEN_MASK
-                        for w, lemma in zip(
-                            counterexample_candidate["tokenized"],
-                            counterexample_candidate["lemmatized"],
-                        )
-                    ]
-
-                    intersection = longest_intersection(s1_masked, s2_masked)
-                    if not intersection:
-                        continue
-
-                    start, end = get_start_and_end_of_sublist(s1_masked, intersection)
-                    first_word = example_candidate["words"][start]
-                    last_word = example_candidate["words"][end]
-                    if (
+                start, end = get_start_and_end_of_sublist(s1_masked, intersection)
+                first_word = example_candidate["words"][start]
+                last_word = example_candidate["words"][end]
+                if (
                         first_word["case"] != "success"
                         or last_word["case"] != "success"
                         or "end" not in last_word
                         or "start" not in first_word
                         or last_word["end"] - first_word["start"]
                         < args.min_phrase_duration
-                    ):
-                        continue
+                ):
+                    continue
 
-                    (counterex_start, counterex_end,) = get_start_and_end_of_sublist(
-                        s2_masked, intersection
+                (counterex_start, counterex_end,) = get_start_and_end_of_sublist(
+                    s2_masked, intersection
+                )
+                first_word = counterexample_candidate["words"][counterex_start]
+                last_word = counterexample_candidate["words"][counterex_end]
+                if (
+                        first_word["case"] != "success"
+                        or last_word["case"] != "success"
+                        or "end" not in last_word
+                        or "start" not in first_word
+                        or last_word["end"] - first_word["start"]
+                        < args.min_phrase_duration
+                ):
+                    continue
+
+                if len(intersection) > len_longest_intersection:
+                    example = crop_and_create_example(
+                        example_candidate.copy(), start, end, lemma_1, lemma_2,
                     )
-                    first_word = counterexample_candidate["words"][counterex_start]
-                    last_word = counterexample_candidate["words"][counterex_end]
-                    if (
-                        first_word["case"] != "success"
-                        or last_word["case"] != "success"
-                        or "end" not in last_word
-                        or "start" not in first_word
-                        or last_word["end"] - first_word["start"]
-                        < args.min_phrase_duration
-                    ):
-                        continue
 
-                    if len(intersection) > len_longest_intersection:
-                        example = crop_and_create_example(
-                            example_candidate.copy(), start, end, lemma_1, lemma_2,
-                        )
+                    counterexample = crop_and_create_example(
+                        counterexample_candidate.copy(),
+                        counterex_start,
+                        counterex_end,
+                        lemma_2,
+                        lemma_1,
+                    )
 
-                        counterexample = crop_and_create_example(
-                            counterexample_candidate.copy(),
-                            counterex_start,
-                            counterex_end,
-                            lemma_2,
-                            lemma_1,
-                        )
+                    len_longest_intersection = len(intersection)
+                    best_example = example
+                    best_counterexample = counterexample
+                    best_counterex_row = row_counterexample
 
-                        len_longest_intersection = len(intersection)
-                        best_example = example
-                        best_counterexample = counterexample
-                        best_counterex_row = row_counterexample
+        if best_example is not None:
+            results.append(best_example)
+            results.append(best_counterexample)
+            print(best_example["tokenized"])
+            print(best_counterexample["tokenized"], end="\n\n")
 
-            if best_example is not None:
-                best_example["id"] = id
-                id += 1
-                best_counterexample["id"] = id
-                id += 1
-                best_example["id_counterexample"] = best_counterexample["id"]
-                best_counterexample["id_counterexample"] = best_example["id"]
+            used_counterexamples.append(best_counterex_row)
 
-                eval_set.append(best_example)
-                eval_set.append(best_counterexample)
-                print(best_example["tokenized"])
-                print(best_counterexample["tokenized"], end="\n\n")
+    return results
 
-                used_counterexamples.append(best_counterex_row)
 
-    eval_set = pd.DataFrame(eval_set)
+def find_minimal_pairs(tuples, data, args):
+    process_args = [
+        (tuple, data.copy(), args)
+        for tuple in tuples
+    ]
+
+    # results = [find_minimal_pairs_for_tuple(*args) for args in process_args]
+    with Pool(processes=8) as pool:
+        results = pool.starmap(
+            find_minimal_pairs_for_tuple,
+            tqdm(process_args, total=len(process_args)),
+        )
+
+    eval_set = pd.DataFrame(list(itertools.chain(*results)))
+
     if len(eval_set) > 0:
+        eval_set.reset_index(drop=True, inplace=True)
+        eval_set["id"] = eval_set.index
+        eval_set["id_counterexample"] = eval_set.id.apply(lambda x: x + 1 if x % 2 == 0 else x - 1)
+
         eval_set.set_index("id", inplace=True)
 
     return eval_set
@@ -429,6 +443,5 @@ if __name__ == "__main__":
             eval_set = eval_set.sort_values(by=['clipDuration'])
 
             file_name = f"eval_set_{fragment}_{pos_name}.csv"
-            file_dir = os.path.join(DATA_EVAL_DIR, f"min_phrase_duration_{args.min_phrase_duration}")
-            os.makedirs(file_dir, exist_ok=True)
-            eval_set.to_csv(os.path.join(file_dir, file_name))
+            os.makedirs(DATA_EVAL_DIR, exist_ok=True)
+            eval_set.to_csv(os.path.join(DATA_EVAL_DIR, file_name))
